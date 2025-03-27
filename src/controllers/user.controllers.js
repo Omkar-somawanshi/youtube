b9156ae6aeb3c34bd489d27f46f293aca83be95f
Omkar-somawanshi <omkarsomwanshi552003@gmail.com>
2025-03-27 12:58:27 +0530

    feat: implement subscription model and update refresh token validation logic

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -129,7 +129,7 @@ const logoutUser = asyncHandler(async (req, res) => {
 const refreshAccessToken=asyncHandler(async(req,res) => {
     refreshToken || req.body.refreshToken
 
-    if (incomingRefreshToken){
+    if (!incomingRefreshToken){
     throw new ApiError(401,"unauthorized request")
 }
    try {
